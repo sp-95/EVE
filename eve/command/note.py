8eec56b91501f44dddc2092f2429d83e94196973
Shashanka Prajapati <shashankap95@gmail.com>
2017-03-18 15:46:08 +0530

    Note Taking debugged

--- eve/command/note.py
+++ eve/command/note.py
@@ -15,35 +15,35 @@ def main(save=False):
         with sr.Microphone() as source:
             audio = r.listen(source)
 
-        try:
-            message = r.recognize_google(audio)
-            punctuations = {
-                            '.': [' period', ' full stop', ' fullstop'],
-                            '?': [' question mark'],
-                            ',': [' comma'],
-                            '!': [' exclamation mark'],
-                            ':': [' colon'],
-                            ';': [' semicolon'],
-                            '"': ['double quote', 'double quotes'],
-                            '-': [' hyphen ', ' dash '],
-                            '_': [' underscore '],
-                            '(': ['opening bracket ', 'bracket open '],
-                            ')': [' closing bracket', ' bracket close']
-                            }
-            for k, v in punctuations.items():
-                for x in v:
-                    message = message.replace(x, k)
-            # print("You said " + message)
-            if save:
-                with open('note.txt', 'w') as f:
-                    f.write(message)
-            return message
-        except sr.UnknownValueError:
-            message = "I am sorry, could you repeat that for me?"
-        except sr.RequestError:
-            message = "Please check your internet connection"
-        except:
-            message = "Oh no! That was unexpected"
+            try:
+                message = r.recognize_google(audio)
+                punctuations = {
+                                '.': [' period', ' full stop', ' fullstop'],
+                                '?': [' question mark'],
+                                ',': [' comma'],
+                                '!': [' exclamation mark'],
+                                ':': [' colon'],
+                                ';': [' semicolon'],
+                                '"': ['double quote', 'double quotes'],
+                                '-': [' hyphen ', ' dash '],
+                                '_': [' underscore '],
+                                '(': ['opening bracket ', 'bracket open '],
+                                ')': [' closing bracket', ' bracket close']
+                                }
+                for k, v in punctuations.items():
+                    for x in v:
+                        message = message.replace(x, k)
+                # print("You said " + message)
+                if save:
+                    with open('note.txt', 'w') as f:
+                        f.write(message)
+                return message
+            except sr.UnknownValueError:
+                message = "I am sorry, could you repeat that for me?"
+            except sr.RequestError:
+                message = "Please check your internet connection"
+            except:
+                message = "Oh no! That was unexpected"
         subprocess.call(['say', message])
 
 
